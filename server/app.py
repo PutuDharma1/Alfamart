c2ab63f72fe639aaea618c9c9c5d65b8f15e2eba
PutuDharma <iputudharma.puspa@gmail.com>
2025-07-31 11:39:44 +0700

    22

--- server/app.py
+++ server/app.py
@@ -16,11 +16,11 @@ app = Flask(__name__)
 
 # Konfigurasi CORS
 cors = CORS(app, resources={
-  r"/*": {
+    r"/*": {
     "origins": [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "alfamart-one.vercel.app"
+      "https://alfamart-one.vercel.app" 
     ]
   }
 })
